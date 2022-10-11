041acfd0c228c30ea4598089ab480a26e990cf69
Anatália Neves Mula <mulaanatalia@gmail.com>
2022-10-11 21:04:17 +0200

    Add mobile button.

--- src/components/header/ButtonMob.js
+++ src/components/header/ButtonMob.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
 export default class ButtonMob extends React.Component{
+    
+    constructor(){
+        super();
+        this.state={/*define o estado*/
+            visNav: 'flex'
+        }
+    }
+    
+    /*Fará a função de toggle*/
+    clickMe(){
+        if(this.state.visNav === 'flex'){
+            this.setState({
+                visNav: 'none'
+            });
+        }else{
+            this.setState({
+                visNav: 'flex'
+            });
+        }
+        document.querySelector(".nav").style.display=this.state.visNav;
+    }
 
     render(){
         return(
-            null
+            <div className='buttonMob' onClick={this.clickMe.bind(this)}>
+                <img src="/img/hamburgermenu_120234 (1).png" alt="Botão Mobile"/>
+            </div>
         );
     }
 }
